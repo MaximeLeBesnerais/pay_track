13284c15e440af5b771a99d15a78496ce11f44b4
Maxime <maxoulebesnerais@epitech.eu>
2025-04-16 17:31:10 +0800

    Add themeColor and themeStyle fields to Subscription class for enhanced customization

--- lib/models/subscription.dart
+++ lib/models/subscription.dart
@@ -2,13 +2,16 @@
 import 'package:rrule/rrule.dart';
 
 enum Category { music, video, gaming, social, income, taxes, other }
-
+enum ThemeColor { blue, green, red, purple, orange }
+enum ThemeStyle { light, dark, system }
 
 class Subscription {
   final String name;
   final DateTime startDate;
   final double amount;
   final Category category;
+  final ThemeColor? themeColor;
+  final ThemeStyle? themeStyle;
   final RecurrenceRule recurrenceRule;
   final DateTime? endDate;
   final String? description;
@@ -20,6 +23,8 @@ class Subscription {
     required this.amount,
     required this.category,
     required this.recurrenceRule,
+    this.themeColor,
+    this.themeStyle,
     this.endDate,
     this.description,
   });
@@ -29,6 +34,12 @@ class Subscription {
         startDate = DateTime.parse(json['startDate']),
         amount = json['price'].toDouble(),
         category = Category.values[json['category']],
+        themeColor = json['themeColor'] != null
+            ? ThemeColor.values[json['themeColor']]
+            : null,
+        themeStyle = json['themeStyle'] != null
+            ? ThemeStyle.values[json['themeStyle']]
+            : null,
         recurrenceRule = RecurrenceRule.fromJson(json['recurrenceRule']),
         endDate = json['endDate'] != null ? DateTime.parse(json['endDate']) : null,
         description = json['description'];
