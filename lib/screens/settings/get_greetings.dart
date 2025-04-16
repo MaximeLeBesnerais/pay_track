3fa84bbc0db21f7e0549319a481ea13ab2952858
Maxime <maxoulebesnerais@epitech.eu>
2025-04-17 03:12:49 +0800

    Refactor greeting logic in TimeInterval class to simplify greeting selection

--- lib/screens/settings/get_greetings.dart
+++ lib/screens/settings/get_greetings.dart
@@ -3,9 +3,9 @@ import 'dart:math';
 class TimeInterval {
   final int start;
   final int end;
-  final List<String> greetings;
+  final String greeting;
 
-  TimeInterval(this.start, this.end, this.greetings);
+  TimeInterval(this.start, this.end, this.greeting);
 
   bool isInInterval(DateTime dateTime) {
     final hour = dateTime.hour;
@@ -18,47 +18,17 @@ class TimeInterval {
 
 String getGreeting() {
   final now = DateTime.now();
-  final random = Random();
 
   final timeIntervals = [
-    TimeInterval(5, 12, [
-      "Salutations from the Dawn Chorus",
-      "Well met in the Dew Hours",
-      "Greetings in the Realm of First Light",
-      "Salutations during the Songbird Symphony",
-      "Well wishes from the Mist Kingdom",
-      "Hail from the Land of Rising Gold",
-    ]),
-    TimeInterval(12, 17, [
-      "Greetings from the Sun's High Throne",
-      "Well met in the Hours of Bright Shadows",
-      "Salutations during the Golden Zenith",
-      "Hail from the Realm of Dancing Heat",
-      "Welcome in the Time of Buzzing Wings",
-      "Well wishes from the Meridian Court",
-    ]),
-    TimeInterval(17, 21, [
-      "Greetings from the Ember Hours",
-      "Well met in the Sunset Kingdom",
-      "Salutations during the Twilight Procession",
-      "Hail from the Copper Sky Realm",
-      "Welcome in the Time of Lengthening Shadows",
-      "Well wishes from the Dusk Parade",
-    ]),
-    TimeInterval(21, 5, [
-      "Greetings from the Starlit Dominion",
-      "Well met in the Realm of Night Whispers",
-      "Salutations during the Owl Parliament",
-      "Hail from the Kingdom of Silver Dreams",
-      "Welcome in the Time of Moon Dancers",
-      "Well wishes from the Velvet Hours",
-    ]),
+    TimeInterval(5, 12, "Good morning"),
+    TimeInterval(12, 17, "Good afternoon"),
+    TimeInterval(17, 21, "Good evening"),
+    TimeInterval(21, 5, "Good night"),
   ];
 
   for (var interval in timeIntervals) {
     if (interval.isInInterval(now)) {
-      final index = random.nextInt(interval.greetings.length);
-      return interval.greetings[index];
+      return interval.greeting;
     }
   }
   return 'Hello there';
