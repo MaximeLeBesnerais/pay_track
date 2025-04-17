c392baecac8d21383cc64f807342e5a95a10c535
Maxime <maxoulebesnerais@epitech.eu>
2025-04-17 15:27:10 +0800

    Refactor HomeScreen layout and improve TableCalendar structure for better readability

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -11,36 +11,60 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   DateTime _focusDate = DateTime.now();
   DateTime _selectedDate = DateTime.now();
+  CalendarFormat _calendarFormat = CalendarFormat.month;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
       body: Center(
-        child: TableCalendar(
-          calendarStyle: CalendarStyle(
-            todayDecoration: BoxDecoration(
-              color: Theme.of(context).buttonTheme.colorScheme?.primary,
-              shape: BoxShape.circle,
+        child: 
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child:  Column(
+          children: [
+            TableCalendar(
+              headerStyle: HeaderStyle(
+                titleCentered: true,
+                formatButtonVisible: false,
+                titleTextStyle: Theme.of(
+                  context,
+                ).textTheme.headlineLarge!.copyWith(
+                  color: Theme.of(context).buttonTheme.colorScheme?.primary,
+                ),
+              ),
+              startingDayOfWeek: StartingDayOfWeek.monday,
+              weekendDays: [],
+              calendarStyle: CalendarStyle(
+                todayDecoration: BoxDecoration(
+                  color: Theme.of(context).buttonTheme.colorScheme?.primary,
+                  shape: BoxShape.circle,
+                ),
+                selectedDecoration: BoxDecoration(
+                  color: Theme.of(context).buttonTheme.colorScheme?.secondary,
+                  shape: BoxShape.circle,
+                ),
+              ),
+              focusedDay: _focusDate,
+              selectedDayPredicate: (day) => isSameDay(day, _selectedDate),
+              onDaySelected: (focusedDay, selectedDay) {
+                setState(() {
+                  _focusDate = focusedDay;
+                  _selectedDate = selectedDay;
+                });
+              },
+              firstDay: DateTime(1980, 1, 1),
+              lastDay: DateTime(2100, 12, 31),
+              calendarFormat: _calendarFormat,
+              onHeaderTapped: (focusedDay) {
+                setState(() {
+                  _calendarFormat = _calendarFormat == CalendarFormat.month
+                      ? CalendarFormat.week
+                      : CalendarFormat.month;
+                });
+              },
             ),
-            selectedDecoration: BoxDecoration(
-              color: Theme.of(context).buttonTheme.colorScheme?.secondary,
-              shape: BoxShape.circle,
-            ),
-          ),
-          focusedDay: _focusDate,
-          selectedDayPredicate: (day) => isSameDay(day, _selectedDate),
-          onDaySelected: (focusedDay, selectedDay) {
-            setState(() {
-              _focusDate = focusedDay;
-              _selectedDate = selectedDay;
-            });
-          },
-          firstDay: DateTime(1980, 1, 1),
-          lastDay: DateTime(2100, 12, 31),
-          calendarFormat: CalendarFormat.month,
-          onHeaderTapped: (focusedDay) => print(focusedDay),
-        ),
+          ],
+        )),
       ),
     );
   }
