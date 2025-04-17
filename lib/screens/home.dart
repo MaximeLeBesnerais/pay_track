0230bdd3514fe5c01ace1d6da0e8e7d16d9daa13
Maxime <maxoulebesnerais@epitech.eu>
2025-04-17 14:48:25 +0800

    Implement calendar functionality in HomeScreen with TableCalendar widget

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,16 +9,37 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  DateTime _focusDate = DateTime.now();
+  DateTime _selectedDate = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
+      appBar: AppBar(title: const Text('Home')),
       body: Center(
-        child: Text(
-          'Home Screen',
-          style: Theme.of(context).textTheme.headlineLarge,
+        child: TableCalendar(
+          calendarStyle: CalendarStyle(
+            todayDecoration: BoxDecoration(
+              color: Theme.of(context).buttonTheme.colorScheme?.primary,
+              shape: BoxShape.circle,
+            ),
+            selectedDecoration: BoxDecoration(
+              color: Theme.of(context).buttonTheme.colorScheme?.onPrimary,
+              shape: BoxShape.circle,
+            ),
+          ),
+          focusedDay: _focusDate,
+          selectedDayPredicate: (day) => isSameDay(day, _selectedDate),
+          onDaySelected: (focusedDay, selectedDay) {
+            setState(() {
+              _focusDate = focusedDay;
+              _selectedDate = selectedDay;
+            });
+          },
+          firstDay: DateTime(1980, 1, 1),
+          lastDay: DateTime(2100, 12, 31),
+          calendarFormat: CalendarFormat.month,
+          onHeaderTapped: (focusedDay) => print(focusedDay),
         ),
       ),
     );
